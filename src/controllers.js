6122fe017a39fab4eadb2fc55cc12a13b1e9aa8b
ItsmeLee <arsmagica90@gmail.com>
2020-09-18 12:45:35 +0900

    Change arrow function into normal function
    Edit the all Anonymous function to all named function in controller.js file
    
    Cause : to meet the challenge conditions

--- src/controllers.js
+++ src/controllers.js
@@ -7,37 +7,67 @@ export const apiV1Router = express.Router();
 export const apiV2Router = express.Router();
 
 // home zone
-userRouter.get("/", userHome);
-userRouter.get("/join", userJoin);
-userRouter.get("/login", userLogin);
-userRouter.get("/confirm-account", uesrConfirmAccount);
+userRouter.get("/", uesrHome);
+userRouter.get("/join", uesrJoin);
+userRouter.get("/login", uesrLogin);
+userRouter.get("/confirm-account", userConfirmAccount);
 
-// user function
-const userHome = (req, res) => res.send("Home");
-const userJoin = (req, res) => res.send("join");
-const userLogin = (req, res) => res.send("login");
-const uesrConfirmAccount = (req, res) => res.send("confirm-account");
-
+// Home Function
+function uesrHome(req, res) {
+  res.send("Home");
+}
+function uesrJoin(req, res) {
+  res.send("Join");
+}
+function uesrLogin(req, res) {
+  res.send("Login");
+}
+function userConfirmAccount(req, res) {
+  res.send("confirm-account");
+}
 // course zone
-courseController.get("/");
-courseController.get("/new");
-courseController.get("/mine");
+courseController.get("/", courseHome);
+courseController.get("/new", courseNew);
+courseController.get("/mine", courseMine);
 
-// course function
-const courseHome = (req, res) => res.send("course home");
-const courseNew = (req, res) => res.send("course new");
-const courseMine = (req, res) => res.send("course mine");
-
+//course Function
+function courseHome(req, res) {
+  res.send("course home");
+}
+function courseNew(req, res) {
+  res.send("course new");
+}
+function courseMine(req, res) {
+  res.send("course mine");
+}
 // api
 
-apiRouter.get("/documentation", (req, res) => res.send("Api Documentation"));
+apiRouter.get("/documentation", apiDocumentation);
 
 // api/v1 zone
 
-apiV1Router.get("/buy", (req, res) => res.send("/api/v1/buy"));
-apiV1Router.get("/refund", (req, res) => res.send("/api/v1/refund"));
+apiV1Router.get("/buy", apiBuy);
+apiV1Router.get("/refund", apiRefund);
 
 // api/v2 zone
 
-apiV2Router.get("/remove", (req, res) => res.send("/api/v2/remove"));
-apiV2Router.get("/edit", (req, res) => res.send("/api/v2/edit"));
+apiV2Router.get("/remove", apiRemove);
+apiV2Router.get("/edit", apiEdit);
+
+// Api Function
+
+function apiDocumentation(req, res) {
+  res.send("Api Documentation");
+}
+function apiBuy(req, res) {
+  res.send("/api/v1/buy");
+}
+function apiRefund(req, res) {
+  res.send("/api/v1/refund");
+}
+function apiRemove(req, res) {
+  res.send("/api/v2/remove");
+}
+function apiEdit(req, res) {
+  res.send("/api/v2/edit");
+}
